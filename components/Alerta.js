0ff88a1dcf23bf54274ff27430aece000d4ed899
Miguel <chanampamiguel@gmail.com>
2021-12-09 23:50:44 -0300

    Subida de archivos con Dropzone

--- components/Alerta.js
+++ components/Alerta.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import AuthContext from '../context/auth/authContext';
+import AppContext from '../context/app/appContext';
 
 const Alerta = () => {
 
@@ -7,9 +8,13 @@ const Alerta = () => {
     const authContext = useContext(AuthContext);
     const { mensaje } = authContext;
 
+    // Acceder al Context de la App
+    const appContext = useContext(AppContext);
+    const { mensaje_archivo } = appContext;
+
     return (
         <div className="bg-red-500 py-2 px-3 w-full my-3 max-w-lg text-center text-white mx-auto">
-            { mensaje }
+            { mensaje || mensaje_archivo }
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import React, { useContext, useEffect } from 'react';
+import Link from 'next/link';
 import Layout from '../components/Layout';
+import Alerta from '../components/Alerta';
+import Dropzone from '../components/Dropzone';
 import AuthContext from '../context/auth/authContext';
+import AppContext from '../context/app/appContext';
 
 const Index = () => {
 
@@ -8,13 +12,48 @@ const Index = () => {
   const authContext = useContext(AuthContext);
   const { usuarioAutenticado } = authContext;
 
+  // Acceder al context de App
+  const appContext = useContext(AppContext);
+  const { mensaje_archivo, url } = appContext;
+
   useEffect(() => {
     usuarioAutenticado();
   }, []);
 
   return (
     <Layout>
-      <h1>Index</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        { url ? (
+          <>
+            <p className="text-center text-2xl mt-10">
+              <span className="font-bold text-red-700 text-3xl uppercase">Tu URL es:</span> { `${process.env.frontendURL}/enlaces/${url}` }
+            </p>
+            <button
+              type="button"
+              className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold mt-10"
+              onClick={ () => navigator.clipboard.writeText(`${process.env.frontendURL}/enlaces/${url}`) }
+            >Copiar Enlace</button>
+          </>
+        ) : (
+          <>
+            { mensaje_archivo && <Alerta /> }
+
+            <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+              <Dropzone />
+              
+              <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+                <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">Compartir archivos de forma sencilla y privada</h2>
+                <p className="text-lg leading-loose">
+                  <span className="text-red-500 font-bold">ReactNodeSend</span> te permite compartir archivos con cifrado de extremo a extremo y un archivo que es eliminado después de ser descargado. Así puedes mantener lo que compartes en privado y asegurarte que tus cosas no permanezcan en línea para siempre.
+                </p>
+                <Link href="/crear-cuenta">
+                  <a className="text-red-500 font-bold text-lg hover:text-red-700">Crea una cuenta para mayores beneficios</a>
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </Layout>
   );
 }
